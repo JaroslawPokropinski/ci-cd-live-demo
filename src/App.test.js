49cf0765c657a748c74694097bb4230893baf503
JaroslawPokropinski <jarek.pokropinski@gmail.com>
2020-05-23 21:51:11 +0200

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders how is ci / cd', () => {
+  const { container  } = render(<App />);
+  const howIsCi = container.querySelector('.how-is-ci');
+  expect(howIsCi).not.toBe(null);
 });
+
+// test('renders that ci is awesome', () => {
+//   const { container  } = render(<App />);
+//   const howIsCi = container.querySelector('.how-is-ci');
+//   expect(howIsCi.textContent).toBe('Awesome');
+// });
